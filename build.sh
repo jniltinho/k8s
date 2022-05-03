bb068100b37ff514208bac9af57feca88de8b03f
Nilton Oliveira <jniltinho@gmail.com>
2022-05-03 09:15:08 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 set -e
 
 ## Build
-docker build --no-cache -t dind-alpine-k8s .
+docker build --no-cache -t ghcr.io/jniltinho/dind-alpine-k8s .
 
 ## Test
 # docker run -t -i -v /var/run/docker.sock:/var/run/docker.sock --rm dind-alpine-k8s bash
